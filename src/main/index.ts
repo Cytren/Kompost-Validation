4252a917bc3bfeffd85150fcb050c7670e79bc2e
Cytren <contact@cytren.com>
2018-05-18 17:17:27 +0100

    Added validationAndTransform

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,6 +3,11 @@ import {Validation, ValidationItem, ValidationType} from "./model/validation";
 import validate from "./validate/index";
 import transform from "./transform";
 
+function validateAndTransform (validation: Validation, model: object): object {
+    validate(validation, model);
+    return transform(validation, model);
+}
+
 export {
-    Validation, ValidationType, ValidationItem, validate, transform
+    Validation, ValidationType, ValidationItem, validate, transform, validateAndTransform
 };
